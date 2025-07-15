c021968a2addb41bf25918a8b9ef65f46ada3740
rynajs <145012957+rynajs@users.noreply.github.com>
2025-07-15 01:58:46 -0400

    init and migration

--- public/index.php
+++ public/index.php
@@ -1,1 +1,9 @@
 <?php
+
+if (PHP_SAPI !== 'cli') {
+    echo '<div style="background:#fffbcc;border:1px solid #f7e79d;
+                padding:8px;margin:0;text-align:center;font-family:Arial, sans-serif;
+                font-size:14px;color:#555;">
+            Temporary file to create pull request with <strong>migrations</strong> branch
+          </div>';
+}
